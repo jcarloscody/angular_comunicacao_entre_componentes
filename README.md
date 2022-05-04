42823826ae2c6047e93fa950c2253ed3668f3f3f
Josue <josuecarlosos2@gmail.com>
2022-05-04 10:41:56 -0300

    Input('')

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Comunição entre Componentes
 
-
+> - [`Input Binding`]() **`@Input()  @Input('nome_qq')`** Aqui o foco é passar atributos para o componente por meio do template. 
+> - [`Input Binding`]() **``**  
+> - [`Input Binding`]() **``** 
+> - [`Input Binding`]() **``** 
+> - [`Input Binding`]() **``** 
+> - [`Input Binding`]() **``** 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InputBindingComponent } from './input-binding/input-binding.component';
+import { ClientComponent } from './input-binding/client/client.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputBindingComponent,
+    ClientComponent
   ],
   imports: [
     BrowserModule,
