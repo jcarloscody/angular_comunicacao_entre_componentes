197747083ce59dfde86e08a1ca29965b84338ced
Josue <josuecarlosos2@gmail.com>
2022-05-05 00:54:28 -0300

    NgOnChanges

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { OnChangesComponent } from './on-changes/on-changes.component';
 import { TimerComponent } from './parent-child/timer/timer.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -15,10 +16,12 @@ import { ClientComponent } from './input-binding/client/client.component';
 import { EventEmitterComponent } from './event-emitter/event-emitter.component';
 import { ChildItemComponent } from './event-emitter/child-item/child-item.component';
 import { ParentChildComponent } from './parent-child/parent-child.component';
-
+import { NameChangesComponent } from './on-changes/name-changes/name-changes.component';
 @NgModule({
   declarations: [
     ParentChildComponent,
+    OnChangesComponent,
+    NameChangesComponent,
     TimerComponent,
     AppComponent,
     InputBindingComponent,
