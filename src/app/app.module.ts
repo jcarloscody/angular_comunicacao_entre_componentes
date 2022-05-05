2690dbed848691148d90d36f0f15e0e00bc91e89
Josue <josuecarlosos2@gmail.com>
2022-05-05 00:22:22 -0300

    Variáveis de Templates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
+import { TimerComponent } from './parent-child/timer/timer.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatCardModule} from "@angular/material/card";
-import {MatIconModule} from "@angular/material/icon";
+import { MatCardModule } from "@angular/material/card";
+import { MatIconModule } from "@angular/material/icon";
+import { MatFormField } from "@angular/material/form-field";
+import {MatProgressBarModule} from "@angular/material/progress-bar";
 
 import { InputBindingComponent } from './input-binding/input-binding.component';
 import { ClientComponent } from './input-binding/client/client.component';
 import { EventEmitterComponent } from './event-emitter/event-emitter.component';
 import { ChildItemComponent } from './event-emitter/child-item/child-item.component';
+import { ParentChildComponent } from './parent-child/parent-child.component';
 
 @NgModule({
   declarations: [
-    
+    ParentChildComponent,
+    TimerComponent,
     AppComponent,
     InputBindingComponent,
     ClientComponent,
@@ -26,7 +31,9 @@ import { ChildItemComponent } from './event-emitter/child-item/child-item.compon
     AppRoutingModule,
     BrowserAnimationsModule,
     MatCardModule,
-    MatIconModule
+    MatIconModule,
+    MatFormField,
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
